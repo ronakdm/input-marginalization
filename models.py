1471ea593f6db53ae5d934b5f433dd436d634ba9
Ronak Mehta <ronakdm@uw.edu>
2021-03-11 23:43:25 -0500

    LM write up with how we will do GPT2 experiment.

--- models.py
+++ models.py
@@ -92,6 +92,7 @@ class LSTM(nn.Module):
         self.loss = criterion(self.logits, y)
         return self
 
+
 class SNLILSTM(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, n_labels, n_rnn_layers):
         super().__init__()
@@ -108,7 +109,7 @@ class SNLILSTM(nn.Module):
             bidirectional=True,
         )
         layered_hidden_dim = hidden_dim * n_rnn_layers * 2
-        layered_hidden_dim *= 2 # since concat 2 sentences
+        layered_hidden_dim *= 2  # since concat 2 sentences
 
         self.dropout_train, self.dropout_embedded = (
             nn.Dropout(p=0.5),
@@ -116,9 +117,7 @@ class SNLILSTM(nn.Module):
         )
         self.linear = nn.Linear(layered_hidden_dim, n_labels)
 
-    def forward(
-        self, sentences, labels
-    ):
+    def forward(self, sentences, labels):
         """
             All is same, except text is a tuple containing the 2 sentences
         """
@@ -136,7 +135,7 @@ class SNLILSTM(nn.Module):
 
             return dropped
 
-        #dropped = torch.cat([encode(s1), encode(s2)], dim=1)
+        # dropped = torch.cat([encode(s1), encode(s2)], dim=1)
         print(encode(s1).shape)
         output = self.linear(dropped)
         return output
