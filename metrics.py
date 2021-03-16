7dc28bb290fc7214cfb8bb84c80fcb5e75a0a238
Ronak Mehta <ronakdm@uw.edu>
2021-03-16 16:24:22 -0400

    continuous colored sentence with adjustable color limit.

--- metrics.py
+++ metrics.py
@@ -136,10 +136,10 @@ def input_marginalization(model, sentence, mlm, target_label=None, num_batches=5
         return att_scores
 
 
-def score_to_color(score):
+def score_to_color(score, color_limit):
 
-    lowest = -4
-    highest = -4
+    lowest = -color_limit
+    highest = color_limit
 
     if score > highest:
         rgb = [255, 0, 0]
@@ -165,7 +165,7 @@ def score_to_color(score):
     return str(rgb[0]), str(rgb[1]), str(rgb[2])
 
 
-def continuous_colored_sentence(sentence, att_scores):
+def continuous_colored_sentence(sentence, att_scores, color_limit=3):
 
     input_ids, _, _ = encode(sentence, "cpu")
     tokenized_sentence = tokenizer.convert_ids_to_tokens(input_ids[0, 1:-1])
@@ -181,7 +181,7 @@ def continuous_colored_sentence(sentence, att_scores):
         else:
             joined.append(0)
 
-        colors = score_to_color(scores[i])
+        colors = score_to_color(scores[i], color_limit)
         colored.append(
             "\033[48;2;{};{};{}m{}\033[0m".format(
                 colors[0], colors[1], colors[2], tokenized_sentence[i],
